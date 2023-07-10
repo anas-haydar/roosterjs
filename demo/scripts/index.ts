a55e02d4049004f5416afe4af76e88d7cf375536
Anas haydar <ahaydar@osdservices.com>
2023-07-10 10:34:42 +0300

    t

--- demo/scripts/index.ts
+++ demo/scripts/index.ts
@@ -1,5 +1,10 @@
 import { mount as mountClassicalEditorMainPane } from './controls/MainPane';
 
-(window as any)['mountKqcRoosterEditor'] = () => {
-    return mountClassicalEditorMainPane(document.getElementById('mainPane'));
+(window as any)['mountKqcRoosterEditor'] = (callback?: Function) => {
+    return mountClassicalEditorMainPane(
+        document.getElementById('mainPane'),
+        callback ? callback() : null
+    );
 };
+
+// mountClassicalEditorMainPane(document.getElementById('mainPane'));
